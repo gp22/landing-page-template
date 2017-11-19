fa4114646794560b2d111dc678d7f4cc84dd0c65
gp22 <gp22@tutanota.com>
2017-11-19 17:40:39 -0500

    Add dropdown menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,14 @@
 import '../css/main.scss';
+
+const dropdown = document.getElementById('dropdown');
+const toggle = document.getElementById('toggle');
+
+toggle.addEventListener('click', () => {
+  const classList = Array.from(dropdown.classList);
+
+  if (classList.includes('nav__dropdown--show')) {
+    dropdown.classList = classList.filter(className => className !== 'nav__dropdown--show');
+  } else {
+    dropdown.className = dropdown.className + ' nav__dropdown--show';
+  }
+})
